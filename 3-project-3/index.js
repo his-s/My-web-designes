013c460e6dc24457bcf78fcada83b7e9f6ae7083
his-s <hishamy42@yahoo.com>
2022-08-30 23:42:02 +0200

    initial commit

---
+++ 3-project-3/index.js
@@ -0,0 +1,11 @@
+const score = document.getElementById("score");
+const question = document.getElementById("question");
+const input = document.getElementById("input");
+const number1 = document.getElementById("number-1");
+const number2 = document.getElementById("number-2");
+
+
+score.innerText = 20;
+number1.innerText = 1;
+number2.innerText = 4;
+input.onchange = 10;
