5e16e1045eeac87d3356f61cc980b71e41d8fa9d
his-s <hishamy42@yahoo.com>
2022-09-06 12:28:31 +0200

    complete projects my-self

---
+++ 13-Random Photos/index.js
@@ -0,0 +1,22 @@
+const imgContainerEl = document.querySelector(".img-container");
+const loadMoreBtn = document.querySelector(".btn");
+let imgsCount = 20;
+function createImg(imgsCount) {
+    for (let index = 0; index < imgsCount; index++) {
+        let img = document.createElement("img");
+        img.src = `https://picsum.photos/300?random=${index}`;
+        imgContainerEl.appendChild(img);
+    }
+
+
+}
+createImg(imgsCount);
+
+loadMoreBtn.addEventListener("click", () => {
+
+    imgsCount += 10;
+    console.log(imgsCount);
+    createImg(imgsCount);
+
+
+});
